00a546292696d1f6c126ad15ff7df1361fe28b61
chvinay2110 <chvinay2110@gmail.com>
2025-11-06 15:52:21 +0530

    admin panel updates

--- list/admin.py
+++ list/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import ToDoModel
 # Register your models here.
-admin.site.register(ToDoModel)
+
+class ToDoModelAdmin(admin.ModelAdmin):
+    list_display = ['id','to_do','to_user','checker','is_hidden']
+    list_filter = ['checker','is_hidden']
+    search_fields = ['to_user__username']
+    list_per_page = 30
+
+admin.site.register(ToDoModel,ToDoModelAdmin)
