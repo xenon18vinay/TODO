db66494400e9f368d63da54c9673889fa0c4fa5f
chvinay2110 <chvinay2110@gmail.com>
2025-11-07 14:27:03 +0530

    prod

--- DjangoProject1/settings.py
+++ DjangoProject1/settings.py
@@ -115,8 +115,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = '/home/admin/domains/fairplay47.club/public_html/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
