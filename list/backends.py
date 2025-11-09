f9eb5c9e02b670e0bfa0e8af51f25ea1601718e8
chvinay2110 <chvinay2110@gmail.com>
2025-11-09 13:35:50 +0530

    added ajax to the site for robust timer tracking even while refresh

---
+++ list/backends.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.backends import ModelBackend, UserModel
+from django.db.models import Q
+
+class EmailOrUsernameBackend(ModelBackend):
+    def authenticate(
+        self, request, username = None, password = None, **kwargs
+    ):
+        try:
+            user= UserModel.objects.get(
+                Q(username__iexact=username) | Q(email__iexact=username)
+            )
+        except UserModel.DoesNotExist:
+            return None
+
+        if user.check_password(password):
+            return None
+
+        return None
+
+    def get_user(self, user_id):
+        try:
+            return UserModel.objects.get(pk=user_id)
+        except UserModel.DoesNotExist:
+            return None
