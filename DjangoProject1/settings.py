ba7eecebd26b1e90c2e3be63c42413f669d57338
chvinay2110 <chvinay2110@gmail.com>
2025-11-21 12:02:42 +0530

    Add environment variable support

--- DjangoProject1/settings.py
+++ DjangoProject1/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+from decouple import config
 import os
 from pathlib import Path
 
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8dnj4lss52_y(4ag*@b=yy-x&a-ub9!2xx@0^^ijp6yrxf-(b#'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG',default=False,cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS',default='').split(',')
 
 
 # Application definition
